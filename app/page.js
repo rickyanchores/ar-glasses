13adf752f5668a69f5acc97f89d7e2447f2a6d7a
RickyAnchores <ricky23anchores@gmail.com>
2024-01-29 15:15:22 +0100

    added Banner

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Nav from "@/components/Nav/Nav.jsx";
 import About from "../pages/About/About.jsx";
 import Home from  "../pages/Home/Home.jsx";
+import Banner from "@/pages/Banner/Banner.jsx";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Nav />
       <Home />
       <About />
+      <Banner />
     </main>
   );
 }
---
+++ pages/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='About p-4 min-h-screen flex justify-around items-center'>
+        <div className="image-box w-[680px] h-[460px]">
+            
+        </div>
+        <div className="text-container">
+            <h3 className="font-semibold">EXPERIENCE AR like never before</h3>
+            <h1 className="text-4xl font-bold">XREAL Air Glasses</h1>
+            <span></span>
+            <button className="btn">Read</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner
